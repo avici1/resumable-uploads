a03a76a1f744c828327ce234bb0a6d05b997b753
Alain Christian <48126545+gal1l0@users.noreply.github.com>
2020-06-22 14:13:50 +0200

    added origin cors config

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import '@babel/polyfill'
 import { Server } from 'node-static';
 import express from 'express';
 import routes from './routes';
-
+import cors from 'cors';
 
 let app = new express();
 let fileServer = new Server('./src/public');
 
-
+app.use(cors());
+app.set('Access-Control-Allow-Origin', '*')
 app.use('/api/v1', routes);
 app.use('/', (req, res) => {
     fileServer.serve(req, res);
